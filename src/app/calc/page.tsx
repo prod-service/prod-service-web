bb35c847547e0a4fb0587f056e29c8ced46fa3d2
prod-service <klop23539@gmail.com>
2025-02-23 20:16:49 +0200

    Database integration. Install prisma lib. Connect neon db

--- src/app/calc/page.tsx
+++ src/app/calc/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-    return <p>Calc Page</p>;
-  }
+import { prisma} from "@/app/lib/db";
+
+export default async function Page() {
+  const users = await prisma.users.findMany();
+
+  return <div>
+    <p>Calc Page</p>;
+    <p>{ users.map((user) => {
+      return <span>{user.name}</span>
+    }) }</p>
+  </div>
+}
---
+++ src/app/lib/placeholder-data.ts
@@ -0,0 +1,14 @@
+export const users = [
+    {
+        id: 1,
+        name: 'Name MY',
+        email: 'omg@gmail.com',
+        password: '1234'
+    },
+    {
+        id: 2,
+        name: 'Name MY 23',
+        email: 'omg_23@gmail.com',
+        password: '1234'
+    },
+];
