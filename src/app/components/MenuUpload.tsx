f1775c1c0e60848553bb88c6a9cefcd3ee8cb951
prod-service <klop23539@gmail.com>
2025-03-09 12:56:41 +0200

    Add xlsx package to the project. Create read xlsx handler

--- src/app/components/MenuUpload.tsx
+++ src/app/components/MenuUpload.tsx
@@ -1,10 +1,25 @@
 'use client'
 
 import FileUpload from "./FileUpload";
+import { read, utils } from "xlsx";
 
 export default function MenuUpload() {
     const handlerUploading = (file: File) => {
         console.log(file);
+        const reader = new FileReader();
+
+        reader.onload = (event) => {
+            const workbook = read(event.target?.result);
+            const sheetName = workbook.SheetNames[0];
+            const sheet = workbook.Sheets[sheetName];
+            const sheetData = utils.sheet_to_json(sheet);
+    
+            console.log(sheetData);
+
+        }
+
+        reader.readAsArrayBuffer(file);
+        
     }
 
     return(
