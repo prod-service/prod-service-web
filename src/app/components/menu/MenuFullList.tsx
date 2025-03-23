4fbdb2e5d6ff4cfa650192992eb7d12f348d1f5b
prod-service <klop23539@gmail.com>
2025-03-23 12:08:19 +0200

    Create reducer for day list with popup option

--- src/app/components/menu/MenuFullList.tsx
+++ src/app/components/menu/MenuFullList.tsx
@@ -1,48 +1,45 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { IMenuObj } from "@/app/lib/menu-table-parser";
 import MenuSingleDay from "./MenuSingleDay";
 import Popup from "../Popup";
 import { getValueByKey } from "@/app/helpers";
 import MealCalcForm from "./MealForm";
+import { dayListReducer } from "@/app/reducers";
+import { DayListActionsKind } from "@/app/reducers/dayListReducer";
 
 interface MenuFullListProps {
     menuObject: IMenuObj
 };
 
-interface IDayListItem {
-    name: string,
-    popupOpened: boolean,
-}
-
 const MenuFullList: React.FC<MenuFullListProps> = ({ menuObject }) => {
-    const [dayList, setDayList] = useState<Array<IDayListItem>>([]);
+    const [dayList, setDayList] = useReducer(dayListReducer, []);
 
-    const togglePopup = (index: number, flag: boolean) => {
-        setDayList((prevValue) => {
-            prevValue[index].popupOpened = flag;
-            return [...prevValue]
-        });
+    const showPopupDay = (index: number): void => {
+        setDayList({ type: DayListActionsKind.SHOW_POPUP, payload: index });
+    };
+    const hidePopupDay = (index: number): void => {
+        setDayList({ type: DayListActionsKind.HIDE_POPUP, payload: index });
     };
     
     useEffect(() => {
-        setDayList(Object.keys(menuObject).map(d => ({ name: d, popupOpened: false })));
+        setDayList({ type: DayListActionsKind.SET_LIST, payload: Object.keys(menuObject) });
     }, [menuObject])
     
     return (
         <ul>
             {dayList.length > 0 && 
                 <ul className="md:grid md:grid-cols-2 md:gap-2 lg:grid-cols-3">
-                    { dayList.map(({ name, popupOpened }, index) => {
+                    { dayList.map(({ name: dayName, popupOpened }, index) => {
                         return <li key={index} className="p-2 md:p-4 mb-4 border-2">
                             <div className="flex justify-between">
-                                <span className="font-bold mr-2">{name}</span>
-                                <button onClick={() => togglePopup(index, true)} className="transition bg-indigo-500 hover:bg-indigo-400 py-1 px-2 rounded text-white">
+                                <span className="font-bold mr-2">{dayName}</span>
+                                <button onClick={() => showPopupDay(index)} className="transition bg-indigo-500 hover:bg-indigo-400 py-1 px-2 rounded text-white">
                                     Відкрити
                                 </button>
                             </div>
-                            <MenuSingleDay dayObject={getValueByKey(name, menuObject)} />
-                            <Popup key={index} isOpen={popupOpened} onClose={() => togglePopup(index, false)}>
-                                <MealCalcForm dayTitle={name} originFormObj={getValueByKey(name, menuObject)} />
+                            <MenuSingleDay dayObject={getValueByKey(dayName, menuObject)} />
+                            <Popup key={index} isOpen={popupOpened} onClose={() => hidePopupDay(index)}>
+                                <MealCalcForm dayTitle={dayName} originFormObj={getValueByKey(dayName, menuObject)} />
                             </Popup>
                         </li>
                     }) }
